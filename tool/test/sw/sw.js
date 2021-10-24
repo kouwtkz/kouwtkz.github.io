521dd0a256edd033496cd114f4787b602839485a
Kou Watakaze <kouran-22-b@hotmail.co.jp>
2021-10-25 01:42:14 +0900

    トップページの仮組とテストページの調整

--- tool/test/sw/sw.js
+++ tool/test/sw/sw.js
@@ -1,42 +1,26 @@
-const CACHE_VERSION = "v1.2";
+const CACHE_VERSION = "v1.4";
 const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
 
 // キャッシュするファイルをセットする
 const urlsToCache = ["."];
-((e) => {
-    if (e !== null)
-        e.innerHTML =
-            "<p>" + CACHE_VERSION + "</p>" + "<p>" + CACHE_NAME + "</p>";
-})(document.getElementById("output"));
 
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener("message", (e) => {
+    e.source.postMessage({
+        cashe_version: CACHE_VERSION,
+        cashe_name: CACHE_NAME,
+        data: e.data
+    });
 });
-
-self.addEventListener("activate", (event) => {
+addEventListener("fetch", (event) => {
     event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return cacheNames.filter((cacheName) => {
-                    // このスコープに所属していて且つCACHE_NAMEではないキャッシュを探す
-                    return (
-                        cacheName.startsWith(`${registration.scope}!`) &&
-                        cacheName !== CACHE_NAME
-                    );
-                });
-            })
-            .then((cachesToDelete) => {
-                return Promise.all(
-                    cachesToDelete.map((cacheName) => {
-                        // いらないキャッシュを削除する
-                        return caches.delete(cacheName);
-                    })
-                );
-            })
+        (async function () {
+            if (!event.clientId) return;
+            const client = await clients.get(event.clientId);
+            if (!client) return;
+            client.postMessage({
+                msg: "call fetch",
+                url: event.request.url,
+            });
+        })()
     );
 });
